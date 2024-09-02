146f0dadb2fbbf3f1b9f9808a5648488f66241f2
navi-04 <naveenrajthiyagarajan6@gmail.com>
2024-09-02 22:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
 
 
+if __name__ == '__main__':
+    
+    print("--------> welcome to the farmers connections <--------")
 
-print("--------> welcome to the farmers connections <--------")
+    while(True):
+        print("\n\nMenu")
+        print("1.Register (for new users only)")
+        print("2.Login (for existing users only)")
+        print("3.Exit")
 
-while(True):
-    print("\n\nMenu")
-    print("1.Register (for new users only)")
-    print("2.Login (for existing users only)")
-    print("3.Exit")
+        ch=int(input("Enter your choice: "))
 
-    ch=int(input("Enter your choice: "))
-
-    if ch==1:
-        continue
-    elif ch==2:
-        print("Login")
-    elif ch==3:
-        print("Thank you for using our application")
-        break
+        if ch==1:
+            continue
+        elif ch==2:
+            print("Login")
+        elif ch==3:
+            print("Thank you for using our application")
+            break
 
 
